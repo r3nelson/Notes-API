ddc8d7abaefd1e9bf42ce00cf37569e7ba46a4c8
Richard Nelson <r3nelson@outlook.com>
2024-09-15 01:48:09 -1000

    added models to py to specify params for database entries

--- backend/models/models.py
+++ backend/models/models.py
@@ -1,21 +1,10 @@
-from pydantic import BaseModel
-from typing import Optional, List
+from sqlalchemy import Column, Integer, String, Float
+from db.database import Base
 
-class FlashCard(BaseModel):
-    questions: str
-    confidence: float
-    notes: str
+class FlashCard(Base):
+    __tablename__ = "flashcards"
 
-class Materials(BaseModel):
-    type: str # book, online course, etc.
-    author: Optional[str] = None
-    book_name: Optional[str] = None
-    course: Optional[str] = None
-    course_name: Optional[str] = None
-    platform: Optional[str] = None # udemy, INE, etc.
-
-class Subject(BaseModel):
-    content_from: str
-    study_material: Materials  
-    continuations:Optional[list] = None
-    flashcards: List[FlashCard]
+    id = Column(Integer, primary_key=True, index=True)
+    question = Column(String, nullable=False)
+    confidence = Column(Float,nullable= False)
+    answer = Column(String, nullable=False)
