df3c0c1298990f4b17f30839d2a558196e2fae15
Richard Nelson <r3nelson@outlook.com>
2024-09-21 14:23:53 -1000

    updated continuations default value to [] instead of None. Removed Materials schemas

--- backend/schemas/schemas.py
+++ backend/schemas/schemas.py
@@ -18,39 +18,11 @@ class FlashCardResponse(BaseModel):
 
 class SubjectCreate(BaseModel):
     name: str
-    continuations: Optional[List[str]] = None
-    # study_material: Optional[List[MaterialsCreate]] = None  
-    # flashcards: Optional[List[FlashCardCreate]] = None
+    continuations: Optional[List[str]] = []
 
 class SubjectResponse(BaseModel):
     id: int
     name: str
-    continuations: Optional[List[str]] = None
-    # study_material: Optional[List[MaterialsResponse]] = None    
-    
-    # flashcards: Optional[List[FlashCardResponse]] = None # this field isn't necessary as long as we can match flashcard by subject_id
-
+    continuations: Optional[List[str]] = []
     class Config:
         orm_mode = True
-
-# class MaterialsCreate(BaseModel):
-#     type: str # book, online course, etc.
-#     author: Optional[str] = None
-#     book_name: Optional[str] = None
-#     course: Optional[str] = None
-#     course_name: Optional[str] = None
-#     platform: Optional[str] = None # udemy, INE, etc.
-#     subject_id: int  # Foreign key reference to Subject
-
-# class MaterialsResponse(BaseModel):
-#     id: int
-#     type: str # book, online course, etc.
-#     author: Optional[str] = None
-#     book_name: Optional[str] = None
-#     course: Optional[str] = None
-#     course_name: Optional[str] = None
-#     platform: Optional[str] = None # udemy, INE, youtube etc.
-#     subject: Optional["SubjectResponse"]  # Include the associated subject
-
-    # class Config:
-    #     orm_mode = True
