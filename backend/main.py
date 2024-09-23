5e722dff85a319986e9c449f2e69302e5c02bece
Richard Nelson <r3nelson@outlook.com>
2024-09-22 22:29:42 -1000

    added cors to main.py to allow connection from frontend

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import flashcards
 from routers import subjects
 from db.database import  init_db
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # might change to specify frontend # allow_origins=["http://localhost:3000"] 
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  
+)
 
 # initalize database
 init_db()
