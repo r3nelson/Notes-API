0801a61ca6e7262a145fe28a97972d8f95f11f98
Richard Nelson <r3nelson@outlook.com>
2024-09-24 08:56:30 -1000

    made subject_id, subject_name optional for flashcards. Made continuations optional for subject

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -3,11 +3,12 @@ export interface FlashCard {
   question: string;
   answer: string;
   confidence: number;
-  subject_id: number;
+  subject_id?: number;
+  subject_name?: string;
 }
 
 export interface Subject {
   id: number;
   name: string;
-  continuations: string[];
+  continuations?: string[];
 }
